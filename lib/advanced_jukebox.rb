8b97cc617449a429e52d481f20f98b5cf7ecbe9a
Andrew Bernier <a6bernier@gmail.com>
2018-09-13 17:10:57 +0000

    automatically backed up by learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -11,64 +11,70 @@
 # "Blue" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/06.mp3',
 # "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
 # }
+require 'pry'
+
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
+def run(songs)
+  help
+  loop do
+    puts "Please enter a command:"
+    command = gets.chomp
+    if command == "help"
+      help
+    elsif command == "list"
+      list(songs)
+    elsif command == "play"
+      play(songs)
+    elsif command == "exit"
+      exit_jukebox
+    end
+    break if command == "exit"
+  end
+end
 
 def help
-  def help
-    puts "I accept the following commands:"
-    puts "- help : displays this help message"
-    puts "- list : displays a list of songs you can play"
-    puts "- play : lets you choose a song to play"
-    puts "- exit : exits this program"
+  puts "I accept the following commands:"
+  puts " - help : displays this help message"
+  puts " - list : displays a list of songs you can play"
+  puts " - play : lets you choose a song to play"
+  puts " - exit : exits this program"
+end
+
+def list(songs)
+  songs.each_with_index do |song, index|
+    puts "#{index+1}. #{song}"
   end
+end
 
-
-  def list(my_songs)
-    my_songs.each do |key,value|
-      puts "#{key}"
+def play(songs)
+  puts "Please enter the song number or name: "
+  selection = gets.chomp
+  if selection.to_i == 0
+    if songs.include?(selection)
+      puts "Playing #{selection}"
+    else
+      puts "Invalid input, please try again"
     end
-  end
-
-  def play(my_songs)
-
-    puts "Please enter a song name or number:"
-    response = gets.chomp
-
-    if my_songs.include?(response)
-      path = my_songs[response]
-      system "open #{path}"
+  else
+    if selection.to_i > 0 && selection.to_i <= songs.length
+      puts "Playing #{songs[(selection.to_i-1)]}"
     else
       puts "Invalid input, please try again"
     end
   end
+end
 
-
-  def exit_jukebox
-    puts "Goodbye"
-  end
-
-  def run(my_songs)
-
-    help
-    puts "Please enter a command:"
-    command = gets.chomp
-    loop do
-        if command == "help"
-          help
-          puts "Please enter a command:"
-          command = gets.chomp
-        elsif command == "list"
-          list(my_songs)
-          puts "Please enter a command:"
-          command = gets.chomp
-        elsif command == "play"
-          play(my_songs)
-          puts "Please enter a command:"
-          command = gets.chomp
-        elsif command == "exit"
-          exit_jukebox
-          break
-        end
-      end
-    end
-  end
+def exit_jukebox
+  puts "Goodbye."
 end
